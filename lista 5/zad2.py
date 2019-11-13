b6e9accd30c040fdd07f309e46bc8fb458d5c397
Cielako <cielica.kamil@gmail.com>
2019-11-13 21:35:06 +0100

    update

--- lista 5/zad2.py
+++ lista 5/zad2.py
@@ -1,10 +1,20 @@
-def zamien():
-    slownik = {"1":"jeden", "2":"dwa","3":"trzy","4":"cztery","5":"pięć", "6":"sześć","7":"siedem","8":"osiem","9":"dziewięć","10":"dziesięć"}
-    liczba = input("Podaj liczbę w postaci cyfr(zakres od 1 do 1999): ")
-    dziesietne=""
-    for i in range(len(liczba)):
-        if i > 0 and slownik[liczba[i]] > slownik[liczba[i-1]]:
-            dziesietne += slownik[liczba[i]]-2*slownik[liczba[i-1]]
-        else: dziesietne += slownik[liczba[i]]
-    print(dziesietne)
-zamien()
+num = int(input("Podaj liczbę z zakresu (1-1999): "))
+while num > 1999 or num <= 0:
+        print("Podana liczba jest nieprawidłowa, spróbuj ponownie !!!")
+        num = int(input("Podaj liczbę z zakresu (1-1999): "))
+def num2words(num):
+	ponizej_20 = ['','zero','jeden','dwa','trzy','cztery','pięć','sześć','siedem','osiem','dziewięć','dziesięć','jedenaśćie','dwanaście','trzynaście','czternaście','piętnaście','szesnaście','siedemnaście','osiemnaście','dziewietnaście']
+	dziesiatki = ['dwadzieścia','trzydzieści','czterdzieśći','pięćdziesiąt','sześćdziesiąt','siedemdziesiąt','osiemdziesiąt','dziewięćdziesiąt']
+	powyzej_100 = {100: 'sto',200: 'dwieście',300: 'trzysta',400: 'Czterysta',500: 'pięćset',600: 'sześćset',700: 'siedemset',800: 'osiemset',900: 'dziewięćset',1000:'Tysiąc',}
+
+	if num < 20:
+		 return ponizej_20[num+1]
+	
+	if num < 100:
+		return dziesiatki[(int)(num/10)-2] + ('' if num%10==0 else ' ' + ponizej_20[num%10+1])
+
+	# element przestawny
+	element = max([key for key in powyzej_100.keys() if key <= num])
+
+	return num2words(int(-1)) + '' + powyzej_100[element] + ('' if num%element==0 else ' ' + num2words(num%element))
+print(num2words(num))
